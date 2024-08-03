b0e485a2aac36eea3c818211d62357068b7a86f8
SumitParira <155645426+SumitParira@users.noreply.github.com>
2024-08-03 09:08:10 +0000

    object

--- .devcontainer/02_basics/03_objects.js
+++ .devcontainer/02_basics/03_objects.js
@@ -8,7 +8,7 @@ const mySym = Symbol("key1")
 const  JsUser = {
     name:"sumit",
     "full name" : "Hitesh Choudhary",
-    mySym : "mykey1",
+    [mySym] : "mykey1",
     age:20,
     location : "Kolkata",
     email: "sumit@gmail.com",
@@ -16,7 +16,22 @@ const  JsUser = {
     lastLoginDays:["Monday","Saturday"]
 }
 
-console.log(JsUser.email);
-console.log(JsUser["email"]);
-console.log(JsUser["full name"]);
-console.log(JsUser.mySym);
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+// console.log(typeof JsUser[mySym]);
+
+JsUser.email = "parirsu@gmail.com"
+//Object.freeze(JsUser)
+JsUser.email="PARIRMICros@gmail.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user ");
+    }
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user,${this.name} `);
+    }
+console.log(JsUser.greeting);
+console.log(JsUser.greetingTwo());
