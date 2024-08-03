437ae48244b3a35384c5929bcf915aba68b5ae01
SumitParira <155645426+SumitParira@users.noreply.github.com>
2024-08-03 18:07:46 +0000

    object2

---
+++ .devcontainer/02_basics/04_objects.js
@@ -0,0 +1,68 @@
+//const tinderUser = new Object()
+const tinderUser = { }
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+// console.log(tinderUser);
+
+const regularUser = {
+    email : "some@gmail.com ",
+        fullname: {
+            userfullname:{
+                firstname: "sumit",
+                lastname: "parira"
+
+            }
+        }
+    }
+    //console.log(regularUser.fullname?.userfullname.firstname);
+
+    const obj1={1:"a",2: "b"}
+    const obj2={3:"a",4: "b"}
+    
+    //const obj3 = {obj1,obj2}
+
+    //const obj3 = Object.assign({},obj1, obj2)
+
+    const obj3 = {...obj1,...obj2}
+    console.log(obj3);
+    console.log(obj1);
+
+const users = [
+    {
+        id:1,
+        email : "h@gmail.com"
+    },{
+        id:1,
+        email:"to@gmail.com"
+    },{
+
+    }
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
+    // const target = {a:1,b:2};
+    // const source = {b:4,c:5};
+
+    // const returnedTarget = Object.assign(target,source);
+
+    // console.log(target);
+
+    // console.log(returnedTarget);
+    
+
+    // console.log(returnedTarget === target);
+    
+
+
