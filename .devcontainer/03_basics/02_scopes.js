c703b3198a7cfa4868d48581a21fb73fb91910dd
SumitParira <155645426+SumitParira@users.noreply.github.com>
2024-08-11 21:05:43 +0000

    scope

--- .devcontainer/03_basics/02_scopes.js
+++ .devcontainer/03_basics/02_scopes.js
@@ -4,9 +4,44 @@ let a =300;
 if(true){
 let a =10;
 const b=20;
-console.log("Inner: ",a);
+//console.log("Inner: ",a);
 }
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
+
+function one(){
+    const username ="sumit";
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+    //two()
+}
+//one()
+
+if(true){
+    const username = "sumit"
+    if(username === "sumit"){
+        const website =" youtube"
+        //console.log(username + website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+
+//++++++++++++++++++++++++ interesting ++++++++++++++++
+addone(5)
+function addone(num){
+    return num +1 
+}
+
+console.log(addTwo(5));
+const addTwo=function(num){
+    return num+2
+}
+
+
