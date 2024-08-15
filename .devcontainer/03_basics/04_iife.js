b71d055c6e0866bb3bcbaadde6fa5651602a3144
SumitParira <155645426+SumitParira@users.noreply.github.com>
2024-08-15 20:57:41 +0000

    IIFE

---
+++ .devcontainer/03_basics/04_iife.js
@@ -0,0 +1,11 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+(function chai(){
+    // name IIFE
+    console.log(`DB CONNECETED`);
+    })();
+
+( (name) =>{
+console.log(`DB CONNECTED TWO ${name}`);
+
+})(`sumit`)
