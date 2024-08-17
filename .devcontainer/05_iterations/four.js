53ef1306bc0d77a80a69be989b97a04e66d50350
SumitParira <155645426+SumitParira@users.noreply.github.com>
2024-08-17 09:22:05 +0000

    for each loop

---
+++ .devcontainer/05_iterations/four.js
@@ -0,0 +1,17 @@
+const myObject={
+    js:'javascript',
+    cpp:'c++',
+    rb:'ruby',
+    swift:'swift'
+}
+
+for (const key in myObject) {
+  //console.log(`object is ${myObject[key]} & key is ${key}. `);
+  }
+
+ const programming =["js","rb","py","java","cpp"]
+
+ for (const key in programming) {
+console.log(programming[key]);
+}
+
